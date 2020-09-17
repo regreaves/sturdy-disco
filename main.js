f5c85496db4f727c0b03bb36356d5c154297905d
Ralph Greaves <60410404+regreaves@users.noreply.github.com>
2020-09-17 02:51:43 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let fetchButton = document.querySelector('.fetch-button');
 
 fetchButton.addEventListener('click', () => {
-  import('./modules/json-fetch-request.js').then((Module) => {
+  import('./modules/customScript.js').then((Module) => {
     let jsonFetchRequest = new Module.JSONFetchRequest('https://raw.githubusercontent.com/regreaves/sturdy-disco/master/users');
+    jsonFetchRequest.fetchJSONFile();
   })
 });
